3dcc4994168e955da38a4b120e92615f2cdbfaad
Viktor Ivannikov <iceivannikov@gmail.com>
2022-07-17 15:27:44 +0300

    Fixed the Runner class. Added Application.run()

--- src/main/java/ru/javarush/cryptoanalyser/ivannikov/Runner.java
+++ src/main/java/ru/javarush/cryptoanalyser/ivannikov/Runner.java
@@ -1,7 +1,9 @@
 package ru.javarush.cryptoanalyser.ivannikov;
 
+import ru.javarush.cryptoanalyser.ivannikov.app.Application;
+
 public class Runner {
     public static void main(String[] args) {
-
+        Application.run();
     }
 }
